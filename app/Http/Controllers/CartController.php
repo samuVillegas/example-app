d3826f133fbc18f32ffa511ba44c1133bca5ba6f
samuVillegas <villegassamuel25@gmail.com>
2023-05-10 07:21:57 -0500

    Changes

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -46,9 +46,12 @@ class CartController extends Controller
 
     public function add(string $id, Request $request): RedirectResponse
     {
+
+
+
         $cartProductData = $request->session()->get('cart_product_data');
         $cartProductData[$id] = $id;
-        $request->session()->put('cart_product_data', $cartProductData);
+        $request->session()->put('cart_product_data', $cartProductData) ;
 
         return back();
     }
